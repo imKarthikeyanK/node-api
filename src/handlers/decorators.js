72e21bd31cb39ad2d14eb0474d265a37003691d6
Karthikeyan <superkarthi789@gmail.com>
2021-08-17 10:31:12 +0530

    update resolver

--- src/handlers/decorators.js
+++ src/handlers/decorators.js
@@ -5,26 +5,36 @@ var service = require('../services/weatherService');
 
 
 function checkExpiryAndLoadData(wrapped) {
-    return function () {
+    return async function () {
         var db = mongodbutil.getDb();
 
         const cities = settings.WEATHER_CITIES;
 
-        cities.map(function (city) {
-            db.collection('cityWiseData1').findOne({ 'city.name': city.toLowerCase() }, '', async (err, result) => {
-                if (err) return console.log(err)
-                if (!result || Math.round((new Date().getTime() - result.expiry) / 60000) > settings.DATA_REFRESH_THRESHOLD_IN_MINS) {
-                    await __loadData(city);
+        let promises = cities.map(function (city) {
+            return new Promise((resolve, reject) => {
+                db.collection('cityWiseData1').findOne({ 'city.name': city.toLowerCase() }, '', async (err, result) => {
+                    if (err) return console.log(err)
+                    if (!result || Math.round((new Date().getTime() - result.expiry) / 60000) > settings.DATA_REFRESH_THRESHOLD_IN_MINS) {
+                        await __loadData(city, resolver);
+                    }
+                    else {
+                        resolve('r')
+                    }
+                });
+                function resolver() {
+                    resolve('r')
                 }
-            });
+            })
         });
 
-        const result = wrapped.apply(this, arguments)
-        return result;
+        await Promise.all(promises).then(() => {
+            const result = wrapped.apply(this, arguments)
+            return result;
+        })
     }
 }
 
-__loadData = async function (city) {
+__loadData = async function (city, callback) {
     var db = mongodbutil.getDb();
 
     let weatherData = service.getData(city);
@@ -37,6 +47,8 @@ __loadData = async function (city) {
                     res.data.city.name = res.data.city.name.toLowerCase();
                     db.collection('cityWiseData1').insertOne(res.data, (err, results) => {
                         if (err) return console.log(err)
+                        console.log("[INFO] Inserted")
+                        callback()
                     });
                 })
             }
